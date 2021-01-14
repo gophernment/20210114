e5eaa49a763c0092d4e262a36d723124a1c0f3a8
Pallat Anchaleechamaikorn <yod.pallat@gmail.com>
2021-01-14 11:54:35 +0700

    refactor to use fmt.Sprintf

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,14 +1,11 @@
 package fizzbuzz
 
+import "fmt"
+
 func Say(n int) string {
 	if n == 3 {
 		return "Fizz"
 	}
-	if n == 2 {
-		return "2"
-	}
-	if n == 4 {
-		return "4"
-	}
-	return "1"
+
+	return fmt.Sprintf("%d", n)
 }
